e2171dcf4fe6431259986798e0a56ac700a47f73
alan-konhevalic <alan.patrick.konhevalic@gmail.com>
2025-04-24 10:11:56 -0300

    adiciona lista tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 import { SomaComponent } from './pages/soma';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SomaComponent, CommonModule, CalculadoraComponent],
+  imports: [RouterOutlet, SomaComponent, CommonModule, CalculadoraComponent, ListaTarefasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
